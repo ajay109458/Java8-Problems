de4995773f56ea25e32deb590dad25052c860a14
ajayyadavmscs <ajayyadav@utexas.edu>
2024-11-24 16:43:23 +0530

    Added java 8 problems

--- src/Day1Problems.java
+++ src/Day1Problems.java
@@ -1,4 +1,8 @@
 import java.util.List;
+import java.util.Optional;
+import java.util.function.BinaryOperator;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class Day1Problems {
 
@@ -23,6 +27,86 @@ public class Day1Problems {
         return fruits.stream().map(String::toUpperCase).sorted().toList();
     }
 
+    /**
+     * Functional Interfaces
+     * Use the Predicate<T> functional interface to check if a string starts with the letter 'A'.
+     *
+     * Input: "Apple", "Banana", "Apricot"
+     * Output: true, false, true
+     */
+    public static List<Boolean> CheckIfStartsWithA(List<String> fruits) {
+        Predicate<String> predicate = input -> input.startsWith("A");
+        return fruits.stream().map(fruit -> predicate.test(fruit)).toList();
+    }
+
+    /**
+     * 5. Optional Class
+     * Write a program to safely retrieve a value from an Optional. If the value is absent, provide a default value.
+     *
+     * Input: Optional.empty()
+     * Output: "Default Value"
+     */
+    public static void printOptionalValues(List<String> fruits) {
+        Optional<String> optinalWithoutValue = Optional.empty();
+        String result = optinalWithoutValue.orElse("Some random value");
+        System.out.println(result);
+    }
+
+    /**
+     *  Method References
+     * Use method references to simplify a program that prints each element of a list.
+     *
+     * Input: [10, 20, 30]
+     * Output:
+     * Copy code
+     * 10
+     * 20
+     * 30
+     */
+    public static void printValueofList(List<Integer> numbers) {
+        numbers.forEach(System.out::println);
+    }
+
+    /**
+     * Find the product of all numbers in a list using the reduce() method.
+     *
+     * Input: [1, 2, 3, 4]
+     * Output: 24
+     */
+    public static void streamReduction(List<Integer> numbers) {
+        numbers.stream().reduce(0, Integer::sum);
+    }
+
+    /**
+     * Use Collectors to group a list of strings by their length.
+     *
+     * Input: ["cat", "dog", "elephant", "bat"]
+     * Output: {3=[cat, dog, bat], 8=[elephant]}
+     */
+    public static void collectorsExample(List<String> animals) {
+        animals.stream().collect(Collectors.groupingBy(String::length));
+    }
+
+    /**
+     * Parallel Streams
+     * Demonstrate how to use parallel streams to sum all elements in a large list of integers efficiently.
+     *
+     * Input: [1, 2, 3, ..., 1000000]
+     * Output: 500000500000
+     */
+    public static Integer sumOfIntegerParallelly(List<Integer> numbers) {
+        return numbers.parallelStream().reduce(0, Integer::sum);
+    }
 
 
+}
+
+/**
+ * Default Methods in Interfaces
+ * Create an interface Calculator with a default method square(int number) that returns the square of a number. Implement the interface and demonstrate the default method.
+ */
+interface TestInferface {
+    default int square(int a, int b) {
+        return a*b;
+    }
 }
